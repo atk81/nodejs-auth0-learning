8fd48034feb99ed55b8be563dd7e3c54099e95dd
atk81 <ashutoshkushwaha81@gmail.com>
2022-01-25 10:15:33 +0530

    Added user into database

--- app.js
+++ app.js
@@ -1,38 +1,61 @@
 require('dotenv').config(); // Load .env file
 require('./config/database').connect(); // Load database configuration
 const express = require('express');
+const User = require('./models/User');
 
 const app = express();
 // Add a middleware to accept json data
 app.use(express.json());
 
-app.get("/register", (req, res) => {
-    const {firstname, lastname, email, password} = req.body;
+app.get("/api/v1/register", async (req, res) => {
+    let {firstname, lastname, email, password} = req.body;
     // Validate the data
     if (!firstname || !lastname || !email || !password) {
-        return res.status(400).json({
+        res.status(400).json({
             message: "Please enter all fields"
         });
+        return ;
     }
     // Check if email already exists
-    const userExits = User.findOne({email: email});
+    // remove all the white spaces from the email
+    email = email.trim();
+    const userExits = await User.findOne({email: email});
     if (userExits) {
-        return res.status(400).json({
+        res.status(400).json({
             message: "Email already exists"
         });
+        return ;
     }
 
     // Valiate password
     if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/.test(password)){
-        return res.send(400).json({
+        res.status(400).json({
             message:`Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number and one special character`
         });
+        return ;
     };
 
-    // Hash the password
+    // Create a new user
+    const user = new User({
+        firstname,
+        lastname,
+        email,
+        password
+    });
+    // TODO: Generate a token
 
-
-
+    // Save the user
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+        return ;
+    }
+    catch (err) {
+        res.status(400).json({
+            message: err.message
+        });
+        return ;
+    }
 })
 
 app.get("/", (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const { Schema } = mongoose;
 
 const userAuthSchema = new Schema({
@@ -18,7 +19,7 @@ const userAuthSchema = new Schema({
                 // Validate email format using regex
                 return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
             },
-            message: props => `{props.value} is not a valid email address!`
+            message: props => `${props.value} is not a valid email address!`
         },
         unique: true
     },
@@ -30,6 +31,18 @@ const userAuthSchema = new Schema({
         type: String,
         default: ''
     },
+});
+
+// Before saving the user, hash the password
+userAuthSchema.pre('save', function(next) {
+    const user = this;
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
 });
 
 module.exports = mongoose.model('User', userAuthSchema);
